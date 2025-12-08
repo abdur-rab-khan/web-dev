4b6a6af3211ad784890a66b567f54ad773077a0f
Abdur Rab Khan <abdurrabkhan3263@gmail.com>
2025-12-08 18:04:50 +0530

    Add ESLint configuration and initial project files
    - Created eslint.config.mjs to define ESLint rules and settings for JavaScript files.
    - Added index.js and index.ts files as entry points for the ESLint project.
    - Introduced intro.md to provide comprehensive documentation on ESLint, its core concepts, configuration, and usage examples.

---
+++ tools/eslint/eslint-project/eslint.config.mjs
@@ -0,0 +1,19 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    name: "JavaScript Files",
+    ignores: ["**/dist/**", "**/node_modules/**"],
+    basePath: "./src",
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+    rules: {
+      "no-unused-vars": "error",
+      semi: "error",
+    },
+  },
+]);
