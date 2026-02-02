7bec678acf458aec0b1df06fafcc762ceb1891e1
Abdur Rab Khan <abdurrabkhan222@gmail.com>
2026-02-02 13:17:20 +0530

    Iterator is completed

--- core/javascript/basics/iterator.js
+++ core/javascript/basics/iterator.js
@@ -1,23 +1,32 @@
 /*
 +-----------------------------------------------------------------------+ ITERATOR +-----------------------------------------------------------------------+
 |                                                                                                                                                          | 
-|  🟡 Iterator in Javascript is an object that defines "iterator protocol" and returns iterator object to traverse over a collection by using "next()"     |
+|  🟡 Iterator in JavaScript is an object that defines "iterator protocol" and returns iterator object to traverse over a collection by using "next()"     |
 |       method, as a result of "next()" method call, it returns an object with two properties: "value" and "done".                                         |
+|                                                                                                                                                          | 
+|  🟡 To make an object iterable we should have to implement "Object.prototype[Symbol.iterator] = generator_function" with custom generator function that  |
+|     make an object iterable and make (for...of, spread operator, destructors) work.                                                                      |
 |                                                                                                                                                          | 
 |  🟡 Generator function is a special type of function that can be exited and later re-entered, maintaining its context (variable bindings) across         |
 |     re-entrances. It is defined using function* syntax and uses "yield" keyword to produce a series of values on demand.                                 | 
 |                                                                                                                                                          | 
-|  🟡 Every time "next()" is called on generator object, it runs until it hits the next "yield" statement. To stop the generator, we can use "return()"    |
-|       method which will terminate the generator and return the given value.                                                                              |
+|  🟡 Every time "next()" function returned by the generator, it runs until it hits the next "yield" statement. To stop the generator, we can use          |
+|     "return()" method which will terminate the generator and return the given value.                                                                     |
 |                                                                                                                                                          | 
-|  🟡 To make any object iterable through "for...of", "spread operator", "destructuring", etc., we need to implement [Symbol.iterator]() method which      |
-|      returns an iterator object. So these "for...of", "spread operator", etc. needs an iterable object to work with.                                     | 
-|                                                                                                                                                          | 
-|  🟡 In Javascript some build-in objects like Array, String, Map, Set, etc., have already [Symbol.iterator]() method implemented in their prototype,     |
+|  🟡 In JavaScript some build-in objects like Array, String, Map, Set, etc., have already [Symbol.iterator]() method implemented in their prototype,      |
 |       making them iterable by default.                                                                                                                   |
 |                                                                                                                                                          | 
-|  🔵 We can build our own custom iterable objects by implementing [Symbol.iterator]() method in them using "generator function"                          |
++----------------------------------------------------------------------------------------------------------------------------------------------------------+
+|                                                                        IMPORTANT NOTE                                                                    |
++----------------------------------------------------------------------------------------------------------------------------------------------------------+
 |                                                                                                                                                          | 
+|                                                                                                                                                          | 
+|  ⭐ If someone ask about iterator in JavaScript, we can say it is a way to tell the JavaScript how to iterate through (for..of, spread operator,         |
+|     destructors etc). To build iteration protocol we use iterator function "function* " and to attach iterator function with data structure we could use | 
+|     "Object.protocol[Symbol.iterator] = custom-generator-function".                                                                                      | 
+|                                                                                                                                                          | 
+|  ⭐ It gives a way to iterator on a given object through "step by step" using "Iterator.next()", "yield value" (return value step by step using next()), | 
+|      return (to stop the iterator).                                                                                                                      |
 |                                                                                                                                                          | 
 +--------------------------------------------------------------------------+ END +-------------------------------------------------------------------------+
 */
